2443d7106fc7016cfa66951b96304c740cc851a7
PalakSen011 <palak.s@amazatic.com>
2024-12-19 11:16:25 +0530

    Delete Day1 directory

--- Day1/App.js
+++
@@ -1,6 +0,0 @@
-const heading = React.createElement("h1", {id: 'heading'}, "hello world from react "); //this is an object.
-console.log(heading);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);//root.render:-takes the object converting into h1 tag
-//  i .e ReactElemnt(Object)=>HTML(Browser Understands)
